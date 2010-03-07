7c3416b557e47c69b7670e32109521ce6388373d
Daniel X Moore <yahivin@gmail.com>
2010-03-07 12:22:53 -0800

    Extracted CSS

--- site/javascripts/thug/console.js
+++ site/javascripts/thug/console.js
@@ -2,8 +2,7 @@
 * This controls the interactive console for testing the Grease
 */
 var IJC = function() {
-  var WIDTH = '300px';
-  
+
   function processSuccess(command, result, self) {
     // Clear Input
     input.val('');
@@ -16,21 +15,8 @@ var IJC = function() {
   // Private Variables
   var callbacks = {keydown: [], command: [processSuccess]};
   
-  var handle = $("<div class='handle'>Interactive JavaScript Console</div>")
-    .css({
-      'width': "100%",
-      'padding':'2px 4px',
-      'background-color': '#222',
-      'overflow': 'hidden'
-    });
-  
-  var output = $("<div class='outputBuffer'></div>")
-    .css({
-      'padding': '4px',
-      'height': '250px',
-      'width': "100%",
-      'overflow': 'auto'
-    });
+  var handle = $("<div class='handle'>Interactive JavaScript Console</div>");
+  var output = $("<div class='outputBuffer'></div>");
     
   var puts = function(text) {
     output.append(document.createTextNode(text));
@@ -40,9 +26,6 @@ var IJC = function() {
   };
   
   var input = $("<input type='text' />")
-    .css({
-      'width': WIDTH
-    })
     .keydown(function(event) {
       $.each(callbacks['keydown'], function(index, callback) {
         callback(event, self);
@@ -50,7 +33,6 @@ var IJC = function() {
     });
     
   var form = $("<form action='#' method='get'></form>")
-    .css('padding', '2px')
     .append(input)
     .append($("<input type='submit' value='Go'/>"));
   
@@ -77,20 +59,12 @@ var IJC = function() {
     return false;
   });
 
-  var chasis = $("<div class='ijc'></div")
+  var chasis = $("<div class='ijc' id='strd6-GreasyThugConsole'></div")
     .append(handle)
     .append(output)
     .append(form)
-    .css({
-      'z-index': '1000',
-      'position': 'absolute',
-      'background-color': 'black',
-      'color': 'green',
-      'font-family': 'Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New',
-      'font-weight': 'bold',
-      'text-align': 'left'
-    })
     .fadeTo("fast", 0.75)
+    .resizable()
     .draggable({
       handle: ".handle",
       stop: function() {
