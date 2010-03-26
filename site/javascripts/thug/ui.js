4fe62ca96c0a63552f1cd0889c246c6085b3cbad
Daniel X Moore <yahivin@gmail.com>
2010-03-25 21:23:36 -0700

    Broke out content script stylesheets

--- site/javascripts/thug/ui.js
+++ site/javascripts/thug/ui.js
@@ -1,57 +1,61 @@
-var UI = {
+var UI = (function() {
+  var imgURL = chrome.extension.getURL("") + "stylesheets/dark-hive/images/"
 
-  button: function UI_button(text, callback) {
-    var button = $("<button />");
-    button.text(text);
-    button.click(callback);
-    
-    return button;
-  },
+  return {
 
-  window: function UI_window(title, options) {
-    var dragStop = options.dragStop || function(){};
-  
-    var handle = $("<div class='handle' />")
-      .text(title)
-    ;
+    button: function UI_button(text, callback) {
+      var button = $("<button />");
+      button.text(text);
+      button.click(callback);
+      
+      return button;
+    },
+
+    window: function UI_window(title, options) {
+      var dragStop = options.dragStop || function(){};
     
-    var content = $("<div class='content' />");
-    
-    var window = $("<div class='strd6-window' />")
-      .append(handle)
-      .append(content)
-      .draggable({
-        handle: ".handle",
-        stop: dragStop
-      })
-      .css({ // TODO Load co-ordinates
-        left: 0,
-        position:"fixed",
-        top: 0
-      })
-    ;
-    
-    var close = $("<a class='window-control' href='#'>X</a>")
-      .click(function() {
-        window.hide();
-        return false;
-      })
-    ;
-    
-    var toggle = $("<a class='window-control' href='#'>_</a>")
-      .click(function() {
-        content.slideToggle();
-        return false;
-      })
-    ;
-    
-    handle.append(close).append(toggle);
-    
-    window.addChild = function(element) {
-      content.append(element);
-      return this;
+      var handle = $("<div class='handle' />")
+        .text(title)
+      ;
+      
+      var content = $("<div class='content' />");
+      
+      var window = $("<div class='strd6-window' />")
+        .append(handle)
+        .append(content)
+        .draggable({
+          handle: ".handle",
+          stop: dragStop
+        })
+        .css({ // TODO Load co-ordinates
+          left: 0,
+          position:"fixed",
+          top: 0
+        })
+      ;
+      
+      var close = $("<a class='window-control' href='#'>X</a>")
+        .click(function() {
+          window.hide();
+          return false;
+        })
+      ;
+      
+      var toggle = $("<a class='window-control' href='#'>_</a>")
+        .click(function() {
+          content.slideToggle();
+          return false;
+        })
+      ;
+      
+      handle.append(close).append(toggle);
+      
+      window.addChild = function(element) {
+        content.append(element);
+        return this;
+      }
+      
+      return window;
     }
-    
-    return window;
   }
-}
+})();
